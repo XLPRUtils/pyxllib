61c4180e13b6882f659ed34bf7a3a1c51c5291d8
code4101 <877362867@qq.com>
2020-08-14 23:37:58 +0800

    ..

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages
 import io
 
-VERSION = '0.0.8'
+VERSION = '0.0.9'
 
 with io.open("README.md", encoding='utf-8') as f:
     long_description = f.read()
