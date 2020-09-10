067764a27a69a6d8d4a967a06eb872a618f7e746
code4101 <877362867@qq.com>
2020-09-10 16:16:21 +0800

    v0.0.18 RunOnlyOnce, performit

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 from setuptools import setup, find_packages
 import io
 
-VERSION = '0.0.17'
+VERSION = '0.0.18'
 
 with io.open("README.md", encoding='utf-8') as f:
     long_description = f.read()
