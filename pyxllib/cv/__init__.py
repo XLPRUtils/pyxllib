03afee3f0950c8db3dddc4918249f72b13c430ad
code4101 <877362867@qq.com>
2020-09-15 11:13:29 +0800

    v0.0.19 pyxllib.image -> pyxllib.cv

---
+++ pyxllib/cv/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author : 陈坤泽
+# @Email  : 877362867@qq.com
+# @Data   : 2020/08/14 22:59
+
+# 确保安装了前置包
+import pyxllib.debug.installer
+# image部分需要依赖的第三方库
+from .installer import *
+# pdf相关解析功能
+from .fitz_ import *
+# 非算法层面的一些简单的图像格式转换功能
+from .imlib import *
+# 几何运算功能，也有相关的透视变换等图像处理
+from .cv import *
