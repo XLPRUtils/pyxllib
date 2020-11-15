ef8458ebd58d01655d31dad002e857a23b498c97
code4101 <877362867@qq.com>
2020-11-15 20:30:17 +0800

    v0.0.40 cvlib: CvImg + PilImg

--- pyxllib/cv/trackbartools.py
+++ pyxllib/cv/trackbartools.py
@@ -10,7 +10,60 @@
 from pyxlib.cv.debugtools import *
 """
 
-from pyxllib.cv import *
+import math
+
+from pyxllib.basic import TicToc
+from pyxllib.cv.cvlib import *
+
+
+class TrackbarTool:
+    """ 滑动条控件组
+    """
+
+    def __init__(self, winname, img, flags=0):
+        img = xlcv.read(img)
+        cv2.namedWindow(winname, flags)
+        cv2.imshow(winname, img)
+        self.winname = winname
+        self.img = img
+        self.trackbar_names = {}
+
+    def imshow(self, img=None):
+        """ 刷新显示的图片 """
+        if img is None:
+            img = self.img
+        cv2.imshow(self.winname, img)
+
+    def default_run(self, x):
+        """ 默认执行器，这个在类继承后，基本都是要自定义成自己的功能的
+
+        TODO 从1滑到20，会运行20次，可以研究一个机制，来只运行一次
+        """
+        kwargs = {}
+        for k in self.trackbar_names.keys():
+            kwargs[k] = self[k]
+        print(kwargs)
+
+    def create_trackbar(self, trackbar_name, count, value=0, on_change=None):
+        """ 创建一个滑动条
+
+        :param trackbar_name: 滑动条名称
+        :param count: 上限值
+        :param on_change: 回调函数
+        :param value: 初始值
+        :return:
+        """
+        if on_change is None:
+            on_change = self.default_run
+        cv2.createTrackbar(trackbar_name, self.winname, value, count, on_change)
+
+    def __getitem__(self, item):
+        """ 可以通过 Trackbars 来获取滑动条当前值
+
+        :param item: 滑动条名称
+        :return: 当前取值
+        """
+        return cv2.getTrackbarPos(item, self.winname)
 
 
 class ErodeTool(TrackbarTool):
@@ -18,6 +71,7 @@ class ErodeTool(TrackbarTool):
 
     HoughLinesPTool('handwriting/C100001448573-002.png')
     """
+
     def __init__(self, img, winname='ErodeTool', flags=1):
         super().__init__(winname, img, flags)
 
@@ -44,6 +98,7 @@ class HoughLinesPTool(TrackbarTool):
 
     HoughLinesPTool('handwriting/C100001448573-002.png')
     """
+
     def __init__(self, img, winname='HoughLinesPTool', flags=1):
         super().__init__(winname, img, flags)
 
