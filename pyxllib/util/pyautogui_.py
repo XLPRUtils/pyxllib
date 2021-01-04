533da293d30948d8de908005db2e16084bf4ea31
code4101 <877362867@qq.com>
2021-01-04 10:10:01 +0800

    v0.0.51 MatchBase

--- pyxllib/util/pyautogui_.py
+++ pyxllib/util/pyautogui_.py
@@ -5,10 +5,7 @@
 # @Date   : 2020/06/06
 
 
-import os
-import re
-import subprocess
-import time
+from pyxllib.basic import *
 
 try:
     import pyautogui
@@ -217,3 +214,75 @@ def lookup_mouse_position2(w1, w2, reverse=False):
         elif k == 'esc':
             break
         time.sleep(0.4)
+
+
+____press_key = """
+TODO 有待进一步封装整理
+
+简化按键映射、简化PressKey和ReleaseKey过程
+"""
+
+if sys.platform == 'win32':
+    import ctypes
+
+    SendInput = ctypes.windll.user32.SendInput
+
+    # C struct redefinitions
+    PUL = ctypes.POINTER(ctypes.c_ulong)
+
+
+    class KeyBdInput(ctypes.Structure):
+        _fields_ = [("wVk", ctypes.c_ushort),
+                    ("wScan", ctypes.c_ushort),
+                    ("dwFlags", ctypes.c_ulong),
+                    ("time", ctypes.c_ulong),
+                    ("dwExtraInfo", PUL)]
+
+
+    class HardwareInput(ctypes.Structure):
+        _fields_ = [("uMsg", ctypes.c_ulong),
+                    ("wParamL", ctypes.c_short),
+                    ("wParamH", ctypes.c_ushort)]
+
+
+    class MouseInput(ctypes.Structure):
+        _fields_ = [("dx", ctypes.c_long),
+                    ("dy", ctypes.c_long),
+                    ("mouseData", ctypes.c_ulong),
+                    ("dwFlags", ctypes.c_ulong),
+                    ("time", ctypes.c_ulong),
+                    ("dwExtraInfo", PUL)]
+
+
+    class Input_I(ctypes.Union):
+        _fields_ = [("ki", KeyBdInput),
+                    ("mi", MouseInput),
+                    ("hi", HardwareInput)]
+
+
+    class Input(ctypes.Structure):
+        _fields_ = [("type", ctypes.c_ulong),
+                    ("ii", Input_I)]
+
+
+    # Actuals Functions
+
+    def PressKey(hexKeyCode):
+        extra = ctypes.c_ulong(0)
+        ii_ = Input_I()
+        ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra))
+        x = Input(ctypes.c_ulong(1), ii_)
+        ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+
+    def ReleaseKey(hexKeyCode):
+        extra = ctypes.c_ulong(0)
+        ii_ = Input_I()
+        ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra))
+        x = Input(ctypes.c_ulong(1), ii_)
+        ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+    # while True:
+    #     PressKey(0x20)
+    #     ReleaseKey(0x20)
+    #     time.sleep(0.1)
