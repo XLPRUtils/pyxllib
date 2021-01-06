650777fdc208fee374e31aa9d79e6eae3fcd62c3
code4101 <877362867@qq.com>
2021-01-06 17:24:48 +0800

    v0.0.52 update ToLabelmeJson

--- pyxllib/basic/__init__.py
+++ pyxllib/basic/__init__.py
@@ -32,17 +32,10 @@ ____other = """
 
 
 class SingletonForEveryClass(type):
-    """Python单例模式(Singleton)的N种实现 - 知乎: https://zhuanlan.zhihu.com/p/37534850
-
-    注意！注意！注意！重要的事说三遍！
-    我的单例类不是传统意义上的单例类。
-    传统意义的单例类，不管用怎样不同的初始化参数创建对象，永远都只有最初的那个对象类。
-    但是我的单例类，为每种不同的参数创建形式，都构造了一个对象。
-    """
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
-        tag = f'{cls}'  # id加上所有参数的影响来控制单例类
+        tag = f'{cls}'
         # 其实转字符串来判断是不太严谨的，有些类型字符串后的显示效果是一模一样的
         # dprint(tag)
         if tag not in cls._instances:
