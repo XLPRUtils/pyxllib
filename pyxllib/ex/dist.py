9b261525305a4036d3d6f8608ecb6ef7bc21aac8
code4101 <877362867@qq.com>
2021-06-30 14:24:07 +0800

    v0.1.17 from pyxllib.xl import ensure_localfile

--- pyxllib/ex/dist.py
+++ pyxllib/ex/dist.py
@@ -1,10 +1,10 @@
 from pyxllib.xl import distribute_package
 
-distribute_package('D:/slns/pyxllib', '0.1.10')
+distribute_package('D:/slns/pyxllib', '0.1.17')
 
 # 注意这个版本机制不太一样，ui界面标题的版本是另设的
 # distribute_package('D:/slns/xllabelme', '1.8')
 
-# distribute_package('D:/slns/xlcocotools', '0.0.2')
+# distribute_package('D:/slns/xlcocotools', '0.0.2.7')  # 还未上传，待上传
 
-# distribute_package('D:/slns/pyxlpr', '0.0.34', 'xlpr')
+# distribute_package('D:/slns/pyxlpr', '0.0.42', 'xlpr')
