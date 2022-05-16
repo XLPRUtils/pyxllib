6b069c53be1dc011bdcc711d9a1b97d756d54ee9
code4101 <877362867@qq.com>
2022-05-16 22:17:51 +0800

    v0.2.14.1 shapely不是必须的

--- xlproject/xlserverlib.py
+++ xlproject/xlserverlib.py
@@ -4,17 +4,19 @@
 # @Email  : 877362867@qq.com
 # @Date   : 2022/05/11 15:09
 
+"""
+厦门理工模式识别与图像理解重点实验室 API服务接口
+"""
+
 import base64
 import requests
 import json
 
 import cv2
-from pyxllib.xlcv import xlcv
+from pyxllib.cv.xlcvlib import xlcv
 
 
 class XlServer:
-    """ 厦门理工模式识别与图像理解重点实验室 API服务接口 """
-
     def __init__(self, host='172.16.170.134', port=5003, token='token'):
         """ 这里默认设了局域网的host，但是token依然是错的，需要拿到正确的token才能运行 """
         self.host = f'http://{host}:{port}'
@@ -44,11 +46,13 @@ class XlServer:
     def rec_singleline(self, im):
         """ 通用的识别一张图的所有文本，并拼接到一起 """
         data = {'image': self._read_image(im), 'mode': 'basicGeneral'}
-        # try 断网操作~
         try:
             r = requests.post(f'{self.host}/api/aipocr', json.dumps(data), headers=self.headers)
             d = json.loads(r.text)
-            text = ' '.join([sp['label']['text'] for sp in d['shapes']])
+            if 'shapes' in d:
+                text = ' '.join([sp['label']['text'] for sp in d['shapes']])
+            else:  # 因图片太小等各种原因，没有识别到结果
+                text = ''
         except requests.exceptions.ConnectionError:
             text = ''
         return text
